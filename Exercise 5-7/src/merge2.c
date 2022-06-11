835ee02a4f17a6eb65500301130a8091629d8639
Kevin Wielander <kw@woschitzgroup.com>
2022-06-11 17:05:01 +0200

    fix merge 2 by using malloc + loop in corank

--- Exercise 5-7/src/merge2.c
+++ Exercise 5-7/src/merge2.c
@@ -1,3 +1,5 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
@@ -7,55 +9,50 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-void corank(int i, double a[], long n, int *j, double b[], long m, int *k) {
-   *j = MIN(i, m);
+void corank(int i, double a[], long n, int *j, double b[], long m, int *k){
+	*j = MIN(i, n);
 	*k = i - *j;
-	long jlow = MAX(0, i-n);
+	long jlow = MAX(0, i-m);
 	long klow = 0;
-	while (1)
-	{
-		if (((*j > 0) & (*k < n)) && a[*j-1] > a[*k])
+	int done = 0;
+	do{
+		if (((*j > 0) && (*k < m)) && a[*j-1] > b[*k])
 		{
-			long d = (*j-jlow)/2 + ((*j-jlow)/2 != 0);
+			long d = (1+*j-jlow)/2;
 			klow = *k;
 			*j = *j - d;
 			*k = *k + d;
 		} 
-		else if (((*k > 0) & (*j < m)) && b[*k-1] >= a[*j])
+		else if (((*k > 0) && (*j < n)) && b[*k-1] >= a[*j])
 		{
-			long d = (*k-klow)/2 + ((*k-klow)/2 != 0); 
+			long d = (1+*k-klow)/2; 
 			jlow = *j;
 			*j = *j + d;
 			*k = *k - d;
 		}
-		else
-			return 1;
-	}
-	return 0;
+		else{
+		done = 1;
+		}
+	} while(!done);
 }
 
 void merge(double a[], long n, double b[], long m, double c[]) {
+  
+  int t = omp_get_max_threads(); // number of blocks (threads)
+  int i;
 
-    int t = omp_get_max_threads();
+  int* coj = malloc((t+1) * sizeof(int));
+  int* cok = malloc((t+1) * sizeof(int));
 
-    int i;
+  #pragma omp parallel for
+  for (i=0; i<=t; i++) {
+    corank(i*(n+m)/t,a,n,&coj[i],b,m,&cok[i]);
+  }
 
-    int coj[t + 1];
-    int cok[t + 1];
-
-    #pragma omp parallel for
-    for (i = 0; i < t; i++) {
-        corank(i * (n + m) / t, a, n, &coj[i], b, m, &cok[i]);
-    }
-
-
-    coj[t] = n;
-    cok[t] = m;
-
-    #pragma omp parallel for
-    for (i = 0; i <= t; i++) {
-        seq_merge1(&a[coj[i]], coj[i + 1] - coj[i],
-                   &b[cok[i]], cok[i + 1] - cok[i],
-                   &c[i * (n + m) / t]);
-    }
+  #pragma omp parallel for
+  for (i=0; i<=t; i++) {
+    seq_merge1(&a[coj[i]], coj[i+1] - coj[i],
+    &b[cok[i]], cok[i+1] - cok[i],
+    &c[i*(n+m)/t]);
+  }
 }
