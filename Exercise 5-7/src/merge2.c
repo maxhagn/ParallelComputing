4a3929ceac67e0ec17835bbb6aeffbfa23c78044
Kevin Wielander <80787874+KevinWielander@users.noreply.github.com>
2022-06-11 15:04:59 +0200

    Update merge2.c
    fix one off error

--- Exercise 5-7/src/merge2.c
+++ Exercise 5-7/src/merge2.c
@@ -6,9 +6,6 @@
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
-// 1 3 5 7 9 11
-// 0 2 4 6 8 10
-// 0 1 2 3 4 5 ..
 
 void corank(int i, double a[], long n, int *j, double b[], long m, int *k) {
     *j = MIN(i, n);
@@ -17,12 +14,12 @@ void corank(int i, double a[], long n, int *j, double b[], long m, int *k) {
     int d;
     int klow;
     if (*j > 0 && *k < m && a[*j - 1] > b[*k]) {
-        d = (1 + *j - jlow) / 2;
+        d = (*j-jlow)/2 + ((*j-jlow)/2 != 0);
         klow = k;
         *j -= d;
         *k += d;
     } else if (*k > 0 && *j < n && b[*k - 1] >= a[*j]) {
-        d = (1 + *k - klow) / 2;
+        d = (*k-klow)/2 + ((*k-klow)/2 != 0);
         jlow = *j;
         *k -= d;
         *j += d;
@@ -48,14 +45,9 @@ void merge(double a[], long n, double b[], long m, double c[]) {
     cok[t] = m;
 
     #pragma omp parallel for
-    for (i = 0; i < t; i++) {
+    for (i = 0; i <= t; i++) {
         seq_merge1(&a[coj[i]], coj[i + 1] - coj[i],
                    &b[cok[i]], cok[i + 1] - cok[i],
                    &c[i * (n + m) / t]);
     }
-    for (int i = 0; i < 12; i++) {
-        fprintf(stderr, "%f \n", c[i]);
-    }
-
-
 }
