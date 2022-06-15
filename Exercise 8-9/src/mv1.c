bf85cd1ea5b26827c027f4bdb7dfe34366bd3ff1
Kevin Wielander <80787874+KevinWielander@users.noreply.github.com>
2022-06-15 18:58:59 +0200

    Update mv1.c
    "clean up"

--- Exercise 8-9/src/mv1.c
+++ Exercise 8-9/src/mv1.c
@@ -15,28 +15,27 @@ void mv(base_t **A, int nrows, int ncols, int nrows_a_loc, int ncols_a_loc,
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int counts[size];
-    int displs[size];
+    int counts[size], disps[size];
     double compX[ncols];
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+        
     MPI_Allgather(&nrows_x_loc, 1, MPI_INT, counts, 1, MPI_INT, MPI_COMM_WORLD);
 
-    base_t *fullvector = (double *) malloc(nrows * sizeof(double));
+    base_t *vector = (double *) malloc(nrows * sizeof(double));
 
-    MPI_Allgather(x, ncols / size, MPI_DOUBLE, fullvector, ncols / size, MPI_DOUBLE, MPI_COMM_WORLD);
+    MPI_Allgather(x, ncols / size, MPI_DOUBLE, vector, (ncols / size), MPI_DOUBLE, MPI_COMM_WORLD);
 
     for (int i = 0; i < size; i++)
     {
-        if (i == 0) {
-            displs[i] = 0;
-        } else {
-            displs[i] = displs[i - 1] + counts[i - 1];
+        if (i != 0) {
+            disps[i] = disps[i - 1] + counts[i - 1];
+        }else {
+            disps[i] = 0;
         }
     }
 
-    MPI_Allgatherv(&x[0], nrows_x_loc, MPI_DOUBLE, &compX, counts, displs,
+    MPI_Allgatherv(&x[0], nrows_x_loc, MPI_DOUBLE, &compX, counts, disps,
                    MPI_DOUBLE, MPI_COMM_WORLD);
 
     for (int i = 0; i < nrows_a_loc; i++) {
