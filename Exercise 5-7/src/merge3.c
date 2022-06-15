29e5f95a767041e02e62175adf3c48f1eb77463d
Maximilian Hagn <maximilian.hagn@gmail.com>
2022-06-15 10:01:24 +0200

    merge3, 8,9

--- Exercise 5-7/src/merge3.c
+++ Exercise 5-7/src/merge3.c
@@ -28,23 +28,23 @@ int rank(double x, double X[], long n) {
 
 void merge(double A[], long n, double B[], long m, double C[]) {
 
-# pragma omp parallel
+    #pragma omp parallel
     {
-# pragma omp single nowait
+        #pragma omp single nowait
         {
             int CUTOFF = (m + n) / (long) omp_get_max_threads();
             int i;
 
             if (n == 0) {
 
-#pragma omp taskloop
+                #pragma omp taskloop
                 for (i = 0; i < m; i++) {
                     C[i] = B[i];
                 }
 
             } else if (m == 0) {
 
-#pragma omp taskloop
+                #pragma omp taskloop
                 for (i = 0; i < n; i++) {
                     C[i] = A[i];
                 }
@@ -59,13 +59,13 @@ void merge(double A[], long n, double B[], long m, double C[]) {
                 int s = rank(A[r], B, m);
                 C[r + s] = A[r];
 
-#pragma omp task shared (A, B, C )
+                #pragma omp task shared (A, B, C)
                 merge(A, r, B, s, C);
 
-#pragma omp task shared (A, B, C )
+                #pragma omp task shared (A, B, C)
                 merge(&A[r + 1], n - r - 1, &B[s], m - s, &C[r + s + 1]);
 
-#pragma omp taskwait
+                #pragma omp taskwait
 
             }
         }
