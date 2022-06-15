33091e5e1c9fae029323044552e0e67a099bb962
Maximilian Hagn <maximilian.hagn@gmail.com>
2022-06-15 10:03:16 +0200

    merge3, 8,9

--- Exercise 5-7/src/merge3.c
+++ Exercise 5-7/src/merge3.c
@@ -5,6 +5,8 @@
 
 #include "merge.h"
 
+long CUTOFF;
+
 int rank(double x, double X[], long n) {
 
     int start = 0;
@@ -25,50 +27,42 @@ int rank(double x, double X[], long n) {
     return count;
 }
 
-
 void merge(double A[], long n, double B[], long m, double C[]) {
-
-    #pragma omp parallel
+# pragma omp parallel
     {
-        #pragma omp single nowait
+# pragma omp single nowait
         {
-            int CUTOFF = (m + n) / (long) omp_get_max_threads();
-            int i;
-
-            if (n == 0) {
-
-                #pragma omp taskloop
-                for (i = 0; i < m; i++) {
-                    C[i] = B[i];
-                }
-
-            } else if (m == 0) {
-
-                #pragma omp taskloop
-                for (i = 0; i < n; i++) {
-                    C[i] = A[i];
-                }
-
-            } else if (n + m < CUTOFF) {
-
-                seq_merge1(A, n, B, m, C);
-
-            } else {
-
-                int r = n / 2;
-                int s = rank(A[r], B, m);
-                C[r + s] = A[r];
-
-                #pragma omp task shared (A, B, C)
-                merge(A, r, B, s, C);
-
-                #pragma omp task shared (A, B, C)
-                merge(&A[r + 1], n - r - 1, &B[s], m - s, &C[r + s + 1]);
-
-                #pragma omp taskwait
-
-            }
+            CUTOFF = (m + n) / (long) omp_get_max_threads();
+            merge_divconq(A, n, B, m, C);
         }
     }
 }
 
+void merge_divconq(double A[], int n, double B[], int m, double C[]) {
+    int i;
+    if (n == 0) {
+        #pragma omp taskloop
+        for (i = 0; i < m; i++) {
+            C[i] = B[i];
+        }
+    } else if (m == 0) { // task parallelize for large m
+        #pragma omp taskloop
+        for (i = 0; i < n; i++) {
+            C[i] = A[i];
+        }
+    } else if (n + m < CUTOFF) {
+        seq_merge1(A, n, B, m, C);
+    } else {
+        int r = n / 2;
+        int s = getRank(A[r], B, m);
+        C[r + s] = A[r];
+
+        #pragma omp task shared (A, B, C )
+        merge_divconq(A, r, B, s, C);
+
+        #pragma omp task shared (A, B, C )
+        merge_divconq(&A[r + 1], n - r - 1, &B[s], m - s, &C[r + s + 1]);
+
+        # pragma omp taskwait
+    }
+}
