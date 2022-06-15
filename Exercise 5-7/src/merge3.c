d45a44975ca1d29ab3b3dce5e050937d22eae261
Maximilian Hagn <maximilian.hagn@gmail.com>
2022-06-15 09:57:49 +0200

    merge3, 8,9

--- Exercise 5-7/src/merge3.c
+++ Exercise 5-7/src/merge3.c
@@ -28,41 +28,47 @@ int rank(double x, double X[], long n) {
 
 void merge(double A[], long n, double B[], long m, double C[]) {
 
-    int CUTOFF = (m+n)/(long)omp_get_max_threads();
-    int i;
+# pragma omp parallel
+    {
+# pragma omp single nowait
+        {
+            int CUTOFF = (m + n) / (long) omp_get_max_threads();
+            int i;
 
-    if (n == 0) {
+            if (n == 0) {
 
-        #pragma omp taskloop
-        for (i = 0; i < m; i++) {
-            C[i] = B[i];
+#pragma omp taskloop
+                for (i = 0; i < m; i++) {
+                    C[i] = B[i];
+                }
+
+            } else if (m == 0) {
+
+#pragma omp taskloop
+                for (i = 0; i < n; i++) {
+                    C[i] = A[i];
+                }
+
+            } else if (n + m < CUTOFF) {
+
+                seq_merge1(A, n, B, m, C);
+
+            } else {
+
+                int r = n / 2;
+                int s = rank(A[r], B, m);
+                C[r + s] = A[r];
+
+#pragma omp task shared (A, B, C )
+                merge(A, r, B, s, C);
+
+#pragma omp task shared (A, B, C )
+                merge(&A[r + 1], n - r - 1, &B[s], m - s, &C[r + s + 1]);
+
+#pragma omp taskwait
+
+            }
         }
-
-    } else if (m == 0) {
-
-        #pragma omp taskloop
-        for (i = 0; i < n; i++) {
-            C[i] = A[i];
-        }
-
-    } else if (n + m < CUTOFF) {
-
-        seq_merge1(A, n, B, m, C);
-
-    } else {
-
-        int r = n / 2;
-        int s = rank(A[r], B, m);
-        C[r + s] = A[r];
-
-        #pragma omp task shared (A ,B , C )
-        merge(A, r, B, s, C);
-
-        #pragma omp task shared (A ,B , C )
-        merge(&A[r + 1], n - r - 1, &B[s], m - s, &C[r + s + 1]);
-
-        #pragma omp taskwait
-
     }
 }
 
