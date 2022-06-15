1e4b56e01da62619713f708a591e7856f64bc95a
Maximilian Hagn <maximilian.hagn@gmail.com>
2022-06-15 21:24:21 +0200

    mv1

--- Exercise 8-9/src/mv1.c
+++ Exercise 8-9/src/mv1.c
@@ -14,36 +14,26 @@ void mv(base_t **A, int nrows, int ncols, int nrows_a_loc, int ncols_a_loc,
     int rank, size;
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    int counts[size];
-    int displs[size];
-    double compX[ncols];
-
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    MPI_Allgather(&nrows_x_loc, 1, MPI_INT, counts, 1, MPI_INT, MPI_COMM_WORLD);
-
+    int xRowsCount[size];
+    int relativeOffset[size];
+    double receiveCount[ncols];
     base_t *fullvector = (double *) malloc(nrows * sizeof(double));
 
-    MPI_Allgather(x, ncols / size, MPI_DOUBLE, fullvector, ncols / size, MPI_DOUBLE, MPI_COMM_WORLD);
+    MPI_Allgather(&nrows_x_loc, 1, MPI_INT, xRowsCount, 1, MPI_INT, MPI_COMM_WORLD);
 
-    for (int i = 0; i < size; i++)
+    relativeOffset[0] = 0;
+    for (int i = 1; i < size; i++)
     {
-        if (i == 0) {
-            displs[i] = 0;
-        } else {
-            displs[i] = displs[i - 1] + counts[i - 1];
-        }
+        relativeOffset[i] = relativeOffset[i - 1] + xRowsCount[i - 1];
     }
 
-    MPI_Allgatherv(&x[0], nrows_x_loc, MPI_DOUBLE, &compX, counts, displs,
-                   MPI_DOUBLE, MPI_COMM_WORLD);
-
+    MPI_Allgatherv(&x[0], nrows_x_loc, MPI_DOUBLE, &receiveCount, xRowsCount, relativeOffset, MPI_DOUBLE, MPI_COMM_WORLD);
     for (int i = 0; i < nrows_a_loc; i++) {
         b[i] = 0;
         for (int j = 0; j < ncols_a_loc; j++) {
-            b[i] += A[i][j] * compX[j];
+            b[i] += A[i][j] * receiveCount[j];
         }
     }
-
 }
